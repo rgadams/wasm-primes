ea5842431d6755948261fd744359efd65ea387b0
Roy Adams <roy.g.adams@gmail.com>
2021-08-27 15:20:05 -0400

    Updated sieve of eratosthenes

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,18 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
-    crate::utils::set_panic_hook();
-
-    let mut prime_array = vec![1; n];
-    prime_array[0] = 0;
-    prime_array[1] = 0;
-    let root_n = js_sys::Math::sqrt(n as f64) as usize;
-    for i in 2..root_n+1 {
-        if prime_array[i] == 1 {
-            let mut j = i.pow(2);
-            while j < n {
-                prime_array[j] = 0;
-                j = j + i;
-            }
+    let mut primes: Vec<bool> = (0..n + 1).map(|num| num == 2 || num & 1 != 0).collect();
+    let mut num = 3usize;
+    while num * num <= n {        
+        let mut j = num * num;
+        while j <= n {
+            primes[j] = false;
+            j += num;
         }
+        num += 2;
     }
-    filter_prime_list(prime_array)
-}
-
-fn filter_prime_list(prime_array: Vec<usize>) -> Vec<usize> {
-    let mut filtered_prime_array = Vec::new();
-    for i in 0..prime_array.len() {
-        if prime_array[i] == 1 {
-            filtered_prime_array.push(i);
-        }
-    }
-    filtered_prime_array
+    primes.into_iter().enumerate().skip(2).filter_map(|(i, p)| if p {Some(i)} else {None}).collect::<Vec<usize>>()
 }
 
 // algorithm Sieve of Eratosthenes is
